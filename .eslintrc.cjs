ad4d6e48c8e5cc8b203734f603dbc49e3109ded9
areketsuku <alexcaldasaixarch@gmail.com>
2025-12-30 15:26:55 +0100

    hotfix: fixed unwanted react configs on eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,34 +4,20 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2021,
     sourceType: "module",
-    ecmaFeatures: {
-      jsx: true,
-    },
   },
   env: {
-    browser: true,
     node: true,
     es2021: true,
   },
-  plugins: ["@typescript-eslint", "react", "react-hooks"],
+  plugins: ["@typescript-eslint"],
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
   ],
-  settings: {
-    react: {
-      version: "detect",
-    },
-  },
-  rules: {
-    "react/react-in-jsx-scope": "off",
-    "react/jsx-no-target-blank": ["error", { allowReferrer: false }],
-    "react/display-name": "off",
-  },
   ignorePatterns: [
     "node_modules/",
     "dist/",
+    "coverage/",
     ".github/",
   ],
 };
