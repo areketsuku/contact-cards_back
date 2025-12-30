b0a15b75b79736fbf9a7b66647249315650af820
areketsuku <alexcaldasaixarch@gmail.com>
2025-12-30 15:13:35 +0100

    feature: added tecnologies for the project

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  transform: {
+    '^.+\\.(ts)$': 'ts-jest',
+  },
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['lcov', 'text']
+};
